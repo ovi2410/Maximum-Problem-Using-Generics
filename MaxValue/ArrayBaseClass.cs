fc0389dab25913f1a0ec15609989a050e148bc60
ovi2410 <guneshwarifegade@gmail.com>
2022-05-09 15:50:11 +0530

    sorting using generics

---
+++ MaxValue/ArrayBaseClass.cs
@@ -0,0 +1,15 @@
+﻿namespace FindMaxValue
+{
+    public class ArrayClassBase
+    {
+        private IEnumerable<object> inputArray;
+
+        public void ToPrint()
+        {
+            foreach (var ele in inputArray)
+            {
+                Console.Write(ele + " ");
+            }
+        }
+    }
+}
